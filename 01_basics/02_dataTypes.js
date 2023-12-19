7f62146ede57b2cf263b6b78112ba482807930b1
SafaRehan <123244485+SafaRehan@users.noreply.github.com>
2023-12-19 10:14:16 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+console.log("hello safa")
+"use strict "; // treat all jscode as newer version
+alert(3+3);
+
+//PRIMITIVE DATA TYPES
+// number =>2 to the power 3 
+// bigint
+//string => ""
+//Boolean => true or false
+// null => standalone value
+//undefined
+// symbol => unique
+// typeof undefined = undefined
+// typeof null = object
+
+
+// NON PRIMITIVE DATA TYPES
+// Object
