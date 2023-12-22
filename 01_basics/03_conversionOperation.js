0dd0dd1a407ea8a5b3f71bac708520debde335a0
SafaRehan <123244485+SafaRehan@users.noreply.github.com>
2023-12-22 11:38:28 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score ="safa"
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber )
+// console.log(valueInNumber)
+
+// "33"= >33
+// "33abc" => NaN
+// true= 1 and false = 0
+
+// let isLoggedIn = 5
+// let booleanLoggedIn = Boolean (isLoggedIn)
+// console.log( booleanLoggedIn);
+// console.log( typeof booleanLoggedIn);
+
+// 1=true and 0 = false
+// ""= false
+// "safa" = true
+
+// let someNumber = 5
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
