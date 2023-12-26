cc6748f2c2f76594e16d6c15bdc115c13b8c29fd
SafaRehan <123244485+SafaRehan@users.noreply.github.com>
2023-12-26 13:50:28 +0000

    Summary of datatypes

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,14 @@
+// PRIMITIVE DATATYPES
+
+//Symbol
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+//BigInt
+const bigNumber = 22334454546565657n
+console.log(typeof bigNumber);
+console.log( bigNumber);
+
+// NON PRIMITIVES
+// ARRAYS , OBJECTS , FUNCTIONS
