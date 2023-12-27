f67e89e6fc65f8ade727babf148c9f00885d923e
SafaRehan <123244485+SafaRehan@users.noreply.github.com>
2023-12-27 17:00:17 +0000

    string and its method

---
+++ 01_basics/05_string.js
@@ -0,0 +1,33 @@
+// STRING
+// const name = "Safa"
+// const repoCount = 50
+
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+
+const newName = new String('safaRehan') //Also a method to declare string typeof "object"
+// console.log(typeof name) // string
+// console.log(typeof newName)
+// console.log(newName[0])
+// console.log(newName.__proto__)
+// console.log(newName.length)
+// console.log(newName.toUpperCase())
+// console.log(newName.charAt(2))
+// console.log(newName.indexOf("f"))
+
+// the diff between splice and substring is in how they treat the negative number
+// const newString = newName.substring(0,4)
+// console.log(newString)
+// const anotherString = newName.slice(-9,4)
+// console.log(anotherString)
+
+// const friendName = "    DEVIL    " 
+// console.log(friendName )
+// console.log(friendName.trim())
+
+// const url = "https://safa.com/safa%20rehan"
+// console.log(url.replace("%20", "-"))
+// console.log(url.includes("safa"))
+// console.log(url.includes("kaif"))
+
+// const fname = 'kaif-raj-ehtesham-sahil-adnan-warish'
+// console.log(fname.split("-"))
